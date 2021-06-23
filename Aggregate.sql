cc8f1bd4e8e78fc626dbacd61c6d7c5d84f36231
Divya <dsrao0712@gmail.com>
2021-06-24 00:55:14 +0530

    aggregate

--- Aggregate.sql
+++ Aggregate.sql
@@ -37,3 +37,19 @@ WHERE COUNTRYCODE = 'JPN'
 
 SELECT MAX(POPULATION) - MIN(POPULATION)
 FROM CITY
+
+-- The Employee table containing employee data for a company is described as follows:
+-- 
+SELECT salary * months AS earnings, COUNT(*)
+FROM Employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
+
+
+-- Query the following two values from the STATION table:
+
+-- The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+-- The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+
+SELECT ROUND(SUM(LAT_N) , 2) AS lat , ROUND(SUM(LONG_W) , 2) AS lon FROM STATION;
