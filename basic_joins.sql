55b916c480c4066ecdc6882424103aa9aea1a498
Divya <dsrao0712@gmail.com>
2021-06-24 18:37:14 +0530

    joins

--- basic_joins.sql
+++ basic_joins.sql
@@ -1,6 +1,5 @@
 -- https://www.hackerrank.com/challenges/asian-population/problem
 -- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
-
 -- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 
@@ -20,3 +19,5 @@ FROM CITY
 JOIN COUNTRY
 ON CITY.COUNTRYCODE = COUNTRY.CODE
 WHERE COUNTRY.CONTINENT = 'Africa'
+
+
--- manhattan_dist.sql
+++ manhattan_dist.sql
@@ -1,4 +1,4 @@
-Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+-- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
 
 SELECT ROUND( ( ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS( MIN(LONG_W) - MAX(LONG_W)) ), 4)
 FROM STATION
